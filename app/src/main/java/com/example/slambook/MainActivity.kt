d53dee13086d82d327f2229703f9011917fbe996
OsieG <jlgandia2@up.edu.ph>
2020-03-04 17:58:23 +0800

    chore: 3rd commit added kt file and some variables

--- app/src/main/java/com/example/slambook/MainActivity.kt
+++ app/src/main/java/com/example/slambook/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.slambook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.slambook.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +14,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.sendButton.setOnClickListener{
+            sendData(it)
+        }
     }
 
-    private fun sendData(){
+    private fun sendData(view: View){
+        binding.apply{
+            slambookEntry?.name= name_edit.text.toString()
 
+            sendButton.visibility= View.GONE
+            messageEdit.visibility= View.GONE
+            messageText.visibility= View.GONE
+            crushEdit.visibility= View.GONE
+            crushText.visibility= View.GONE
+            dreamEdit.visibility= View.GONE
+            dreamText.visibility= View.GONE
+            courseEdit.visibility= View.GONE
+            courseText.visibility= View.GONE
+            emailEdit.visibility= View.GONE
+            emailText.visibility= View.GONE
+            numberEdit.visibility= View.GONE
+            numberText.visibility= View.GONE
+            birthdayEdit.visibility= View.GONE
+            birthdayText.visibility= View.GONE
+            ageEdit.visibility= View.GONE
+            ageText.visibility= View.GONE
+            nicknameEdit.visibility= View.GONE
+            nicknameText.visibility= View.GONE
+            nameEdit.visibility= View.GONE
+            nameText.visibility= View.GONE
+            helloText.visibility= View.GONE
+            nameHolder.visibility= View.VISIBLE
+            nicknameHolder.visibility= View.VISIBLE
+            ageHolder.visibility= View.VISIBLE
+            messageConst.visibility= View.VISIBLE
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/slambook/SlambookEntry.kt
@@ -0,0 +1,3 @@
+package com.example.slambook
+
+data class SlambookEntry( var name: String= "")
